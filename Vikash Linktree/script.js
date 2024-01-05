8f695d2696a1292b0d609b7ecc6fd3e629f534b3
Vikash Kumar <99268264+Vikashkushwaha70@users.noreply.github.com>
2024-01-06 00:50:56 +0530

    Add files via upload

---
+++ Vikash Linktree/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+	// Initialize Typed.js
+	var typed = new Typed(".typing", {
+		strings: ["a Ux Designer", "a Tech Enthusiast", "an Engineer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		cursorChar: ' |',
+		smartBackspace: true,
+		loop: true
+	});
+};
